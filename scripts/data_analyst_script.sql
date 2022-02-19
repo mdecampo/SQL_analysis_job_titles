dadfe7892c7018dab9966f5a03bdbba64ea23b44
mdecampo <mdecampo@gmail.com>
2022-02-19 13:37:43 -0800

    through bonus

--- scripts/data_analyst_script.sql
+++ scripts/data_analyst_script.sql
@@ -24,8 +24,8 @@ WHERE location IN ('TN','KY')
 /*4. How many postings in Tennessee have a star rating above 4?
 SELECT count(title)
 FROM data_analyst_jobs
-WHERE star_rating>4
-Answer: 416*/
+WHERE star_rating>4 AND location='TN'
+Answer: 3*/
 
 /*5. How many postings in the dataset have a review count between 500 and 1000?
 SELECT count(*) 
@@ -36,8 +36,8 @@ Answer: 151 jobs*/
 /*6. Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?
 SELECT location AS state, round(avg(star_rating),2) AS avg_rating
 FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
 GROUP BY state
-HAVING avg(star_rating) IS NOT NULL
 ORDER BY avg(star_rating) DESC
 Answer: NE has highest rating (4.20)*/
 
@@ -53,28 +53,56 @@ WHERE location='CA'
 Answer:230*/
 
 /*9. Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
-SELECT count(distinct company)
+SELECT count(distinct company), round(avg(star_rating),2)
 FROM data_analyst_jobs
-where review_count>5000
-Answer:40 companies*/
+where review_count>5000*/
+
+/*SELECT company, round(avg(star_rating),2)
+FROM data_analyst_jobs
+where review_count>5000 AND company IS NOT NULL
+GROUP BY company
+--Answer:40 companies*/
 
 /*10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 Select company, round(avg(star_rating),1) as avg_star
 FROM data_analyst_jobs
-WHERE review_count>5000
+WHERE review_count>5000 AND company IS NOT NULL
 GROUP BY company
 ORDER BY avg(star_rating) DESC
 Answer: 6 companies have a tied 4.2 rating. Ordering brings General Motors 4.2 to the top. */
 
-/*11. Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
-SELECT count(title)
+/*--11. Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+SELECT count(distinct title)
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst %'
-Answer:385 jobs*/
+WHERE title ILIKE '%Analyst%'
+Answer:774 jobs*/
 
 /*12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 SELECT title
 from data_analyst_jobs
 WHERE title NOT ILIKE '%Analy%'
-Answer: 4 jobs, word: Tableau */
 
+SELECT title, skill
+from data_analyst_jobs
+WHERE title NOT ILIKE '%Analy%'
+--Answer: 4 jobs, word: Tableau*/
+
+/* BONUS: You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
+
+Disregard any postings where the domain is NULL.
+Order your results so that the domain with the greatest number of hard to fill jobs is at the top.
+Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?*/
+
+/*SELECT count(title) AS job_count, domain
+from data_analyst_jobs
+WHERE skill like '%SQL%' AND days_since_posting>21 AND domain IS NOT NULL
+GROUP BY domain
+ORDER BY count(title) DESC
+
+Answer: Internet/Software, Banks/Financial Services, Consulting, Healthcare*/
+/*SELECT count(title) AS job_count, domain
+from data_analyst_jobs
+WHERE skill like '%SQL%' AND days_since_posting>21 AND domain IS NOT NULL
+GROUP BY domain
+ORDER BY count(title) DESC
+LIMIT 4*/
